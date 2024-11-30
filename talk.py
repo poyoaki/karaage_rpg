eff345c9fe3f1c7643934c701782a0b54abb86d4
Aki Yama <87703386+YukikazeAki@users.noreply.github.com>
2024-11-30 16:46:06 +0900

    fix gbloa is not defined

--- talk.py
+++ talk.py
@@ -5,7 +5,7 @@ import karaage_main
 
 def talk_start():
 
-    util.get_box_left_on_alignment
+    gbloa = util.get_box_left_on_alignment
     pg.draw.rect(karaage_main.screen, (0,0,0), pg.Rect(util.talk_bg_left, util.talk_bg_top, util.talk_bg_width, util.talk_bg_height))
     fork_big = pg.transform.scale(data.fork_img, (250, 250))
     fork_width,fork_height = fork_big.get_size()
