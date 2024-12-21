efa51c07c866e4beb4dedfe169f8c35e689ebc89
to35 <okoshi.tomoharu@nkg.ac.jp>
2024-12-21 16:57:19 +0900

    aaa

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+
+import util
+import data
+import battle
+import shop
+
+#help you
+
+# 画像などは同じフォルダに置く
+dpath = os.path.dirname(__file__)+"/"
+pg.init()
+
+screen_x = 1920
+screen_y = 1080
+#screen = pg.display.set_mode((screen_x, screen_y),pg.FULLSCREEN)
+screen = pg.display.set_mode((screen_x, screen_y))
